cef9df20a4e6bcb86557ccf879e0aaaf1c583638
Taushik Ahmed Joy <taushikjoy@gmail.com>
2022-12-08 01:29:32 +0600

    initital commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
 }
---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const about = (props: Props) => {
+  return <div>about page hello</div>;
+};
+
+export default about;
